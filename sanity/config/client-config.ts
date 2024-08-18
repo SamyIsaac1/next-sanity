e771262f992a27835b39d88c9e75127b44118734
Samy Isaac <60799236+SamyIsaac1@users.noreply.github.com>
2024-08-18 16:54:46 +0300

    feat: edit sanity-utils

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -2,7 +2,7 @@ const config = {
   projectId: 'sv0rqnzh',
   dataset: 'production',
   apiVersion: '2024-08-18',
-  useCdn: true,
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 };
 
 export default config;
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,25 +1,32 @@
-import { createClient, groq } from 'next-sanity';
+import { createClient, groq, type QueryParams } from 'next-sanity';
 import { Project } from '@/types/Project';
 import clientConfig from './config/client-config';
 import { Page } from '@/types/Page';
 
-export async function getProjects(): Promise<Project[]> {
-  return createClient(clientConfig).fetch(
-    groq`*[_type == "project"]{
-      _id,
-      _createdAt,
-      name,
-      "slug": slug.current,
-      "image": image.asset->url,
-      url,
-      content
-    }`
-  );
+export const client = createClient(clientConfig);
+
+export async function sanityFetch<QueryResponse>({
+  query,
+  params = {},
+  revalidate = 10, // default revalidation time in seconds
+  tags = [],
+}: {
+  query: string;
+  params?: QueryParams;
+  revalidate?: number | false;
+  tags?: string[];
+}) {
+  return client.fetch<QueryResponse>(query, params, {
+    next: {
+      revalidate, // for simple, time-based revalidation
+      tags, // for tag-based revalidation
+    },
+  });
 }
 
-export async function getProject(slug: string): Promise<Project> {
-  return createClient(clientConfig).fetch(
-    groq`*[_type == "project" && slug.current == $slug][0]{
+export async function getProjects(): Promise<Project[]> {
+  return sanityFetch({
+    query: groq`*[_type == "project"]{
       _id,
       _createdAt,
       name,
@@ -28,30 +35,44 @@ export async function getProject(slug: string): Promise<Project> {
       url,
       content
     }`,
-    { slug }
-  );
+  });
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  return sanityFetch({
+    query: groq`*[_type == "project" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`,
+    params: { slug },
+  });
 }
 
 export async function getPages(): Promise<Page[]> {
-  return createClient(clientConfig).fetch(
-    groq`*[_type == "page"]{
+  return sanityFetch({
+    query: groq`*[_type == "page"]{
       _id,
       _createdAt,
       title,
       "slug": slug.current
-    }`
-  );
+    }`,
+  });
 }
 
 export async function getPage(slug: string): Promise<Page> {
-  return createClient(clientConfig).fetch(
-    groq`*[_type == "page" && slug.current == $slug][0]{
+  return sanityFetch({
+    query: groq`*[_type == "page" && slug.current == $slug][0]{
       _id,
       _createdAt,
       title,
       "slug": slug.current,
       content
     }`,
-    { slug }
-  );
+    params: { slug },
+  });
 }
